80dce5abfe1da53e98a7ee9ca27b6ec1222eae4c
Makarand Chikhale <makarandchikhale19@gmail.com>
2023-08-31 18:53:09 +0530

    Updates

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import companyReducer from "./slice/company";
-
-export const store = configureStore({
-  reducer: {
-    companyData : companyReducer
-  }
-})
